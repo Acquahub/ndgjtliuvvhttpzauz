c34639ed4a18c5713033588f30b7118ad0f10f7b
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-12 22:59:24 -0500

    My commit message

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,6 +4,8 @@ import styles from './contact.module.css';
 import { Metadata } from "next";
 
 import Image from 'next/image';
+import * as fonts from '../../agua/assets/fonts';
+
 
 
 export const metadata: Metadata = {
@@ -19,13 +21,73 @@ function Contact() {
         <div className={styles["Contact"]}>
             
 <div id="layer-4-3ca79c2c21d04d8bb027fe51c4c746ad" className={styles["layer-4-3ca79c2c21d04d8bb027fe51c4c746ad"]}    >
+<div id="layer-4-942e260bda15444f9e00c7c7d1ad2c11" className={styles["layer-4-942e260bda15444f9e00c7c7d1ad2c11"]}    >
 <div id="layer-6-a25c51ff34514e8e8e2e87f525da2084" className={styles["layer-6-a25c51ff34514e8e8e2e87f525da2084"]}    >
 <div className={styles["imageview-2-118b982191f6499b958356cc1853c91e"]}  >
     <Image id="imageview-2-118b982191f6499b958356cc1853c91e" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-2" src='/assets/Santiago Black and White - Agua.png' />
 </div>
 </div>
 </div>
-<div id="layer-5-39eda45f6f7a417a8f30d954daeb6452" className={styles["layer-5-39eda45f6f7a417a8f30d954daeb6452"]}    ></div>
+<div id="layer-5-bc49def183364e399c5142a03cfa5754" className={styles["layer-5-bc49def183364e399c5142a03cfa5754"]}    >
+<div id="layer-5-051812e1df6a499ab124aa404f625bf8" className={styles["layer-5-051812e1df6a499ab124aa404f625bf8"]}    >
+
+<div className={""}>
+    <div id="text-3-3938acbfa2f049c2acd2d4e09242edf2" className={styles["text-3-3938acbfa2f049c2acd2d4e09242edf2"]}   >
+        Santiago Sáenz Ariza
+    </div>
+</div>
+
+<div className={""}>
+    <div id="text-4-702fdfa665a14a35bc911326d439b0b5" className={styles["text-4-702fdfa665a14a35bc911326d439b0b5"]}   >
+        CEO & Cofounder
+    </div>
+</div>
+</div>
+<div id="layer-6-384714086505432085b7f74de51c8f8d" className={styles["layer-6-384714086505432085b7f74de51c8f8d"]}    >
+<div id="layer-11-3ec5128aa543474e8e429816b96d8fc6" className={styles["layer-11-3ec5128aa543474e8e429816b96d8fc6"]}    >
+<span id="6e59fb80de2847fba070302c01dd20fb" className={styles["icon-1-6e59fb80de2847fba070302c01dd20fb"]}  >extension</span>
+
+<div className={""}>
+    <div id="text-5-5590c2d68e99451e90a3fdfdc17de616" className={styles["text-5-5590c2d68e99451e90a3fdfdc17de616"]}   >
+        [Text]
+    </div>
+</div>
+</div>
+<div id="layer-12-c1603675572b4826b19cdb977b2da180" className={styles["layer-12-c1603675572b4826b19cdb977b2da180"]}    >
+<div className={styles["imageview-2-b16a55caf250497b9a6307669a65b4fc"]}  >
+    <Image id="imageview-2-b16a55caf250497b9a6307669a65b4fc" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-2" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+</div>
+</div>
+<div id="layer-13-a33715f4255647178e8d52a374fa4483" className={styles["layer-13-a33715f4255647178e8d52a374fa4483"]}    >
+<span id="f6a0a619a06744c29886db1d40857eb4" className={styles["icon-2-f6a0a619a06744c29886db1d40857eb4"]}  >extension</span>
+
+<div className={""}>
+    <div id="text-6-a2633b58bf194beab843548bbff6333f" className={styles["text-6-a2633b58bf194beab843548bbff6333f"]}   >
+        [Text]
+    </div>
+</div>
+</div>
+</div>
+<div id="layer-7-3e892d4bb807477d8448ffcf18572432" className={styles["layer-7-3e892d4bb807477d8448ffcf18572432"]}    ></div>
+<div id="layer-8-2099cff669c34a0b987e31fb8f3ea052" className={styles["layer-8-2099cff669c34a0b987e31fb8f3ea052"]}    >
+<div id="layer-10-e3861be832c746bc892b8048db25a533" className={styles["layer-10-e3861be832c746bc892b8048db25a533"]}    >
+
+<div className={""}>
+    <div id="text-1-e2512aa41c0243c381c7a13f0a540ebf" className={styles["text-1-e2512aa41c0243c381c7a13f0a540ebf"]}   >
+        Redes Sociales
+    </div>
+</div>
+
+<div className={""}>
+    <div id="text-2-0fd8ef2db68d4625bc1991009d104c6f" className={styles["text-2-0fd8ef2db68d4625bc1991009d104c6f"]}   >
+        Agua
+    </div>
+</div>
+</div>
+<div id="layer-9-ede60fbe6dc34e3696535c5caa9afa04" className={styles["layer-9-ede60fbe6dc34e3696535c5caa9afa04"]}    ></div>
+</div>
+</div>
+</div>
         </div>
     );
 }
