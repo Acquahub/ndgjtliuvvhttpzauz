9a41eaa504a266373b442a41ca1d2897c7a84d9b
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-21 12:27:27 -0500

    My commit message

--- src/app/screen1/page.tsx
+++ src/app/screen1/page.tsx
@@ -16,92 +16,103 @@ function Screen1() {
     
     
     const openInNewTab = url => {
-    window.open(url, '_blank', '');
+    window.open(url, '_blank', 'noopener,noreferrer');
 };
+    
 
-    return(
-        <div className={styles["Screen1"]}>
-            
-<div id="layer-1-1dcd25da24084328ae85bc8546bebe1d" className={styles["layer-1-1dcd25da24084328ae85bc8546bebe1d"]}    >
-<div id="layer-14-534f3abe8d0746be9978984bc654680a" className={styles["layer-14-534f3abe8d0746be9978984bc654680a"]}    >
-<div id="layer-3-11ea6e88806e4778b7d73e4b032151d8" className={styles["layer-3-11ea6e88806e4778b7d73e4b032151d8"]}    >
-<div className={styles["imageview-1-bd258b9bb61b489dac9f0d5a0ac4f461"]}  >
+    function aguaRender(){
+        return(
+            <div className={"baseScreen hide-native-scrollbar " }>
+                
+<div id="layer-1-1dcd25da24084328ae85bc8546bebe1d" className={"baseLayer hide-native-scrollbar " + styles["layer-1-1dcd25da24084328ae85bc8546bebe1d"]}    >
+<div id="layer-14-534f3abe8d0746be9978984bc654680a" className={"baseLayer hide-native-scrollbar " + styles["layer-14-534f3abe8d0746be9978984bc654680a"]}    >
+<div id="layer-3-11ea6e88806e4778b7d73e4b032151d8" className={"baseLayer hide-native-scrollbar " + styles["layer-3-11ea6e88806e4778b7d73e4b032151d8"]}    >
+<div className={"baseImageView " + styles["imageview-1-bd258b9bb61b489dac9f0d5a0ac4f461"]}  >
     <Image id="imageview-1-bd258b9bb61b489dac9f0d5a0ac4f461" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-1" src='/assets/Santiago Black and White - Agua.png' />
 </div>
 </div>
 </div>
-<div id="layer-13-2721b0c71228410c9daff7c722b54422" className={styles["layer-13-2721b0c71228410c9daff7c722b54422"]}    >
+<div id="layer-13-2721b0c71228410c9daff7c722b54422" className={"baseLayer hide-native-scrollbar " + styles["layer-13-2721b0c71228410c9daff7c722b54422"]}    >
 
 <div className={""}>
-    <div id="text-1-7f816348c5f04ed2b29164e6981daf13" className={styles["text-1-7f816348c5f04ed2b29164e6981daf13"]}   >
+    <div id="text-1-7f816348c5f04ed2b29164e6981daf13" className={"baseText " + styles["text-1-7f816348c5f04ed2b29164e6981daf13"]}   >
         Santiago
     </div>
 </div>
 
 <div className={""}>
-    <div id="text-7-98fb2e24c9c74e00b59d8fedbcc42e70" className={styles["text-7-98fb2e24c9c74e00b59d8fedbcc42e70"]}   >
+    <div id="text-7-98fb2e24c9c74e00b59d8fedbcc42e70" className={"baseText " + styles["text-7-98fb2e24c9c74e00b59d8fedbcc42e70"]}   >
         Sáenz Ariza
     </div>
 </div>
 </div>
 </div>
-<div id="layer-2-30a576cadd594c0c8d642b225d0e9402" className={styles["layer-2-30a576cadd594c0c8d642b225d0e9402"]}    >
-<div id="layer-4-53f94c064dec4155997e8cd8a8542eca" className={styles["layer-4-53f94c064dec4155997e8cd8a8542eca"]}    >
+<div id="layer-2-30a576cadd594c0c8d642b225d0e9402" className={"baseLayer hide-native-scrollbar " + styles["layer-2-30a576cadd594c0c8d642b225d0e9402"]}    >
+<div id="layer-4-53f94c064dec4155997e8cd8a8542eca" className={"baseLayer hide-native-scrollbar " + styles["layer-4-53f94c064dec4155997e8cd8a8542eca"]}    >
 
 <div className={""}>
-    <div id="text-3-250a7b0ed8244774a858e0d0ddd8d791" className={styles["text-3-250a7b0ed8244774a858e0d0ddd8d791"]}   >
+    <div id="text-3-250a7b0ed8244774a858e0d0ddd8d791" className={"baseText " + styles["text-3-250a7b0ed8244774a858e0d0ddd8d791"]}   >
         Agua
     </div>
 </div>
 
 <div className={""}>
-    <div id="text-2-546d2a9764a44f9a94221d28e3bff9d0" className={styles["text-2-546d2a9764a44f9a94221d28e3bff9d0"]}   >
+    <div id="text-2-546d2a9764a44f9a94221d28e3bff9d0" className={"baseText " + styles["text-2-546d2a9764a44f9a94221d28e3bff9d0"]}   >
         CEO & Cofounder
     </div>
 </div>
 </div>
-<div id="layer-6-744d7ac159a74080928dc8468c729e8e" className={styles["layer-6-744d7ac159a74080928dc8468c729e8e"]}    >
-<div id="layer-7-bc13514b025d4b5eac495bc5453d1ece" className={styles["layer-7-bc13514b025d4b5eac495bc5453d1ece"]}    >
-<span id="25eed2b91d444292b8790fee97977c80" className={styles["icon-1-25eed2b91d444292b8790fee97977c80"]}  >local_phone</span>
+<div id="layer-6-744d7ac159a74080928dc8468c729e8e" className={"baseLayer hide-native-scrollbar " + styles["layer-6-744d7ac159a74080928dc8468c729e8e"]}    >
+<div id="layer-7-bc13514b025d4b5eac495bc5453d1ece" className={"baseLayer hide-native-scrollbar " + styles["layer-7-bc13514b025d4b5eac495bc5453d1ece"]}    >
+<span id="25eed2b91d444292b8790fee97977c80" className={"baseIcon " + styles["icon-1-25eed2b91d444292b8790fee97977c80"]}  >local_phone</span>
 
 <div className={""}>
-    <div id="text-4-8857318789e04948ac18c311b23f38ca" className={styles["text-4-8857318789e04948ac18c311b23f38ca"]}   >
-        Llamada
+    <div id="text-4-8857318789e04948ac18c311b23f38ca" className={"baseText " + styles["text-4-8857318789e04948ac18c311b23f38ca"]}   >
+        +57 3214780277
     </div>
 </div>
 </div>
-<div id="layer-8-c26f4a952fdf467fbd92e50ae538f371" className={styles["layer-8-c26f4a952fdf467fbd92e50ae538f371"]}    >
-<div className={styles["imageview-2-1fda112c34f44d198182ed20eed3cc7b"]}  onClick={() => openInNewTab("https://wa.me/573214780277")}>
+<div id="layer-8-c26f4a952fdf467fbd92e50ae538f371" className={"baseLayer hide-native-scrollbar " + styles["layer-8-c26f4a952fdf467fbd92e50ae538f371"]}    >
+<div className={"baseImageView " + styles["imageview-2-1fda112c34f44d198182ed20eed3cc7b"]}  onClick={() => openInNewTab("https://wa.me/573214780277")}>
     <Image id="imageview-2-1fda112c34f44d198182ed20eed3cc7b" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-2" src='/assets/whatsapp.png' />
 </div>
-</div>
-<div id="layer-9-780b5703fede4494b6db488e9738c74e" className={styles["layer-9-780b5703fede4494b6db488e9738c74e"]}    >
-<span id="424e0137dd404e9e931016b5c69c6d35" className={styles["icon-2-424e0137dd404e9e931016b5c69c6d35"]}  >mail</span>
 
 <div className={""}>
-    <div id="text-5-c838c31781fb45ffbb7ffececc60c5bd" className={styles["text-5-c838c31781fb45ffbb7ffececc60c5bd"]}   >
-        Correo
+    <div id="text-8-6a0123913cfd4a798c0a6df66be5af3a" className={"baseText " + styles["text-8-6a0123913cfd4a798c0a6df66be5af3a"]}   >
+        WhatsApp
+    </div>
+</div>
+</div>
+<div id="layer-9-780b5703fede4494b6db488e9738c74e" className={"baseLayer hide-native-scrollbar " + styles["layer-9-780b5703fede4494b6db488e9738c74e"]}    >
+<span id="424e0137dd404e9e931016b5c69c6d35" className={"baseIcon " + styles["icon-2-424e0137dd404e9e931016b5c69c6d35"]}  >mail</span>
+
+<div className={""}>
+    <div id="text-5-c838c31781fb45ffbb7ffececc60c5bd" className={"baseText " + styles["text-5-c838c31781fb45ffbb7ffececc60c5bd"]}   >
+        santiago@agua.app
     </div>
 </div>
 </div>
 </div>
-<div id="layer-10-3e68b5ae3646478aa2e351f0e222fdc5" className={styles["layer-10-3e68b5ae3646478aa2e351f0e222fdc5"]}    >
-<div id="layer-11-4d4bdddc629045cbb59eb4fbe69e9dfe" className={styles["layer-11-4d4bdddc629045cbb59eb4fbe69e9dfe"]}    ></div>
+<div id="layer-10-3e68b5ae3646478aa2e351f0e222fdc5" className={"baseLayer hide-native-scrollbar " + styles["layer-10-3e68b5ae3646478aa2e351f0e222fdc5"]}    >
+<div id="layer-11-4d4bdddc629045cbb59eb4fbe69e9dfe" className={"baseLayer hide-native-scrollbar " + styles["layer-11-4d4bdddc629045cbb59eb4fbe69e9dfe"]}    ></div>
 </div>
-<div id="layer-12-77964a7acd234369949075f2baf3a074" className={styles["layer-12-77964a7acd234369949075f2baf3a074"]}    >
-<div className={styles["imageview-3-5dafe37d8222453a8810fd03f405b21a"]}  >
+<div id="layer-12-77964a7acd234369949075f2baf3a074" className={"baseLayer hide-native-scrollbar " + styles["layer-12-77964a7acd234369949075f2baf3a074"]}    >
+<div className={"baseImageView " + styles["imageview-3-5dafe37d8222453a8810fd03f405b21a"]}  >
     <Image id="imageview-3-5dafe37d8222453a8810fd03f405b21a" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-3" src='/assets/Black Normal.png' />
 </div>
 
 <div className={""}>
-    <div id="text-6-932299da82fb46bd92435af1ce7539bb" className={styles["text-6-932299da82fb46bd92435af1ce7539bb"]}   >
+    <div id="text-6-932299da82fb46bd92435af1ce7539bb" className={"baseText " + styles["text-6-932299da82fb46bd92435af1ce7539bb"]}   >
         www.agua.app
     </div>
 </div>
 </div>
 </div>
-        </div>
-    );
+            </div>
+        );
+    };
+
+    return aguaRender();
 }
 
 export default Screen1;
